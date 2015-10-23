fbcb46d333414abfeff8dac3b5d10b3b28538cf0
johndoan0 <johndoan@Sons-MacBook-Air.local>
2015-10-23 12:23:55 -0500

    profile head added

--- js/BlogView.js
+++ js/BlogView.js
@@ -25,6 +25,19 @@ var BlogView = React.createClass({
 		return(
 			<div id="blogView">
 				<Header />
+				<div type='button' id='profileWraper' onClick={this._profileIconKeyPress}> 
+                    <img src="https://cdn-images-1.medium.com/fit/c/72/72/1*dmbNkD5D-u45r44go_cf0g.png"/>
+                    <span className='userName'></span>
+                </div>
+                <div id="editViewContainer"> 
+                    <h1 id="editH1">
+                        <span>Title</span>
+                    </h1>
+                    <p id="editP">
+                        <span>Tell your story...</span>
+                    </p>
+                    <button>Edit Profile</button>
+                </div>
 				<div id="blogReadWrite">
 					<BlogBoard blogPosts={this.props.blogPosts}/>
 					<BlogInput 
